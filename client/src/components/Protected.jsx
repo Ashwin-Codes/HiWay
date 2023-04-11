ef548f662e3da2c6616b94b86d2102eba508f326
Ashwin Bhargava <contact@ashwincodes.com>
2023-04-12 00:14:43 +0530

    refactor: make Protected.jsx functional via redux store

--- client/src/components/Protected.jsx
+++ client/src/components/Protected.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getAuth } from "../features/auth/authSlice";
 
 export default function Protected({ children }) {
-	// COMPONENT IS NOT FUNCTIONAL YET
-	return <div>{children}</div>;
+	const auth = useSelector(getAuth);
+	if (auth.username && auth.accessToken) {
+		return <div>{children}</div>;
+	}
+	return <Navigate to={"/login"} />;
 }
