1d78e1bba7ff0a4a8966f95c987971edbcc4ae32
Ashwin Bhargava <contact@ashwincodes.com>
2023-04-11 23:23:56 +0530

    feat: add client routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,29 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Protected from "./components/Protected";
+
+// Pages
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
-	return <div className="App"></div>;
+	return (
+		<Routes>
+			<Route path="/">
+				<Route
+					index
+					element={
+						<Protected>
+							<Home />
+						</Protected>
+					}
+				/>
+				<Route path="login" element={<Login />} />
+				<Route path="signup" element={<Register />} />
+				<Route path="*" element={<Navigate to={"/"} />} />
+			</Route>
+		</Routes>
+	);
 }
 
 export default App;
---
+++ client/src/components/Protected.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function Protected({ children }) {
+	// COMPONENT IS NOT FUNCTIONAL YET
+	return <div>{children}</div>;
+}
